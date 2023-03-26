9b5b30800d8dbcf17c59137ee79bdfcc063178de
Ben <ben@thedesigncollaborative.co>
2023-03-26 17:04:15 +0100

    Create Business context and Site schema using generation script

---
+++ lib/voicepilot/business/site.ex
@@ -0,0 +1,21 @@
+defmodule Voicepilot.Business.Site do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "sites" do
+    field :text, :string
+    field :title, :string
+    field :url, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(site, attrs) do
+    site
+    |> cast(attrs, [:title, :url, :text])
+    |> validate_required([:title, :url, :text])
+    |> unique_constraint(:user_id)
+  end
+end
---
+++ priv/repo/migrations/20230326154943_create_sites.exs
@@ -0,0 +1,16 @@
+defmodule Voicepilot.Repo.Migrations.CreateSites do
+  use Ecto.Migration
+
+  def change do
+    create table(:sites) do
+      add :title, :string
+      add :url, :string
+      add :text, :text
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create unique_index(:sites, [:user_id])
+  end
+end
